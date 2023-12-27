2f9c6c18ebda7dc42b9aa3eaa6d010bd9718dfd2
Mohamed Saad <78237246+MohamedSaad0@users.noreply.github.com>
2023-12-27 23:01:43 +0200

    added front template step 2

--- resources/views/user/layout/layouts-script.blade.php
+++ resources/views/user/layout/layouts-script.blade.php
@@ -1,11 +1,18 @@
     <!-- JavaScript Libraries -->
-   @yield('scripts')
+    @yield('scripts')
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+    {{-- <script src="{{ asset('resources/admin/assets/vendor/libs/jquery/jquery.js') }}"></script> --}}
+
+    {{-- <script src="{{ asset('resources/admin/assets/vendor/libs/jquery/jquery.js') }}"></script> --}}
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/waypoints/waypoints.min.js"></script>
-    <script src="lib/lightbox/js/lightbox.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <script src="{{ asset('resources/user/assets/lib/easing/easing.min.js') }}"></script>
+    <script src="{{ asset('resources/user/assets/lib/waypoints/waypoints.min.js') }}"></script>
+    <script src="{{ asset('resources/user/assets/lightbox/js/lightbox.min.js') }}"></script>
+    <script src="{{ asset('resources/user/assets/lib/owlcarousel/owl.carousel.min.js') }}"></script>
 
     <!-- Template Javascript -->
-    <script src="js/main.js"></script>
+    <script src="{{ asset('resources/user/assets/js/main.js') }}"></script>
